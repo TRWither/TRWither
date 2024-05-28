ebc48b96c9881a1b2a9ceb32d2d949221c08e851
Wither__ <163687438+TRWither@users.noreply.github.com>
2024-05-28 18:45:42 +0200

    The strangest way to make a hello world

---
+++ helloworld_but.py
@@ -0,0 +1,28 @@
+char1 = 'H'
+char2 = 'e'
+char3 = 'l'
+char4 = 'l'
+char5 = 'o'
+char6 = ' '
+char7 = 'w'
+char8 = 'o'
+char9 = 'r'
+char10 = 'l'
+char11 = 'd'
+char12 = '!'
+
+def hello():
+  print(char1, end='')
+  print(char2, end='')
+  print(char3, end='')
+  print(char4, end='')
+  print(char5, end='')
+  print(char6, end='')
+  print(char7, end='')
+  print(char8, end='')
+  print(char9, end='')
+  print(char10, end='')
+  print(char11, end='')
+  print(char12)
+
+hello()
