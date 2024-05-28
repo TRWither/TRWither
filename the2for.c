61d9cacc96246d2395036a7231cc150f6e8b3df7
Wither__ <163687438+TRWither@users.noreply.github.com>
2024-05-28 18:50:17 +0200

    for is while

---
+++ the2for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// for loop #1
+for (int i=1; i<=5; i++) {
+  printf("This is the first FOR loop.\n");
+}
+
+// for loop #2
+int i = 1;
+while (i <= 5) {
+  printf("This is the second FOR loop.\n");
+  i++;
+}
